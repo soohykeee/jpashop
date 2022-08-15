b20be1ecd5bb1e0581b8ab773bc81c2a7291d000
soohykeee <gur1028@skuniv.ac.kr>
2022-08-15 17:18:47 +0900

    220815 고급매핑 BaseEntity 추가 및 Superclass로 모든 entity에 추가 + Item Entity 하위에 Album, Book, Movie 추가 및 상속

--- src/main/java/jpabook/jpashop/JpaMain.java
+++ src/main/java/jpabook/jpashop/JpaMain.java
@@ -1,7 +1,6 @@
 package jpabook.jpashop;
 
-import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.Book;
 
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -21,11 +20,12 @@ public class JpaMain {
         tx.begin();
 
         try {
-            Order order = new Order();
-            em.persist(order);
 
-            order.addOrderItem(new OrderItem());
+            Book book = new Book();
+            book.setName("JPA");
+            book.setAuthor("kim");
 
+            em.persist(book);
 
             tx.commit();
         } catch (Exception e) {
